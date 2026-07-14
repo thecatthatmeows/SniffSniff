034adeec8f0417ec93bcf79d16726e27fbbc3bc7
catthatmeows <zairysbigtae@gmail.com>
2026-07-14 21:05:04 +0800

    first sniffing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use pcap::Capture;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Capturing");
+    let mut cap = Capture::from_device("wlp0s20f3").unwrap()
+        .promisc(true)
+        .snaplen(65535)
+        .open()
+        .unwrap();
+
+    println!("Link type: {}", cap.get_datalink().get_name().unwrap_or("Unknown".to_string()));
+
+    while let Ok(packet) = cap.next_packet() {
+        let data = packet.data;
+        for chunk in data.chunks(16) {
+            let hex: Vec<String> = chunk.iter()
+                .map(|b| format!("{:02x}", b)).collect();
+            println!("{}", hex.join(" "));
+        }
+    }
 }
